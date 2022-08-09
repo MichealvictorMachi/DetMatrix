ae34241ebd83ee9fe60b101cbf6b8ac99bf74c5e
Victor Micheal <michealvictor175@gmail.com>
2022-08-09 22:39:58 +0100

    Add project files.

---
+++ DetMatrix/DetMain.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixDeterminant
+{
+    internal class DetMatrix
+    {
+        static void Main(string[] args)
+        {
+                                
+            Determinant determinantFourD = new Determinant();
+            Console.WriteLine("What is the size of the array for determinant?");
+            determinantFourD.ArrayLength = Convert.ToInt32(Console.ReadLine());
+            int arrayLen = determinantFourD.ArrayLength;
+
+            int[,] fourDimArray = determinantFourD.ArrayValues(arrayLen);
+            Console.WriteLine("The determinant is {0}", determinantFourD.Calculation(fourDimArray));
+
+        }
+    }
+}
